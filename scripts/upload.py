4a300f24be5d9542937c62a06dfa5fa79e519bf3
Le-Chat-BottE <56674968+Le-Chat-BottE@users.noreply.github.com>
2023-04-05 20:56:22 +0800

    Update upload.py

--- scripts/upload.py
+++ scripts/upload.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 def on_ui_tabs():
   with gr.Blocks() as upload:
-    gr.File()
+    folder_path = gr.Textbox(label="Folder")
   return (upload, 'Upload', 'upload'),
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
